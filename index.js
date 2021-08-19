4b84d15ed22fa9f3e372c7e2fab865eb93f9de3a
SNIKRO <snikr@bk.ru>
2021-08-19 09:25:52 +0600

    Setup sqlite3, add open/close db

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('todo.sqlite');
 const app = express();
+
+db.serialize(() => {
+  db.run('CREATE TABLE lorem (info TEXT)');
+});
+
+db.close();
 
 app.get('/', (_request, response) => {
   response.send('<h2>HELLO WORLD!</h2>');
