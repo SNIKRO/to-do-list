8be6a4a93d53a46757cb9dff3d443585cfa0df93
SNIKRO <snikr@bk.ru>
2021-08-27 23:41:21 +0600

    Update CRUD for items

--- src/controllers/items/index.js
+++ src/controllers/items/index.js
@@ -11,7 +11,10 @@ router.get('/:listId/items', (request, response) => {
   } = request.query;
 
   db.all(
-    'SELECT * FROM item INNER JOIN list ON item.list_id = list.id WHERE item.list_id = ? AND list.user_id = ? LIMIT ? OFFSET ? ',
+    `SELECT * FROM item 
+    INNER JOIN list ON item.list_id = list.id 
+    WHERE item.list_id = ? AND list.user_id = ? 
+    LIMIT ? OFFSET ? `,
     [1, request.params.listId, limit, offset],
     (error, rows) => {
       if (error) {
@@ -20,7 +23,9 @@ router.get('/:listId/items', (request, response) => {
         return;
       }
       db.get(
-        'SELECT Count(id) as total FROM item INNER JOIN list ON item.list_id = list.id WHERE item.list_id = ? AND list.user_id = ?',
+        `SELECT Count(id) as total FROM item 
+        INNER JOIN list ON item.list_id = list.id 
+        WHERE item.list_id = ? AND list.user_id = ?`,
         [request.params.listId],
         (err, count) => {
           if (err) {
@@ -43,49 +48,103 @@ router.get('/:listId/items', (request, response) => {
 });
 // get single item by id
 router.get('/:listId/items/:itemID', (request, response) => {
-  db.get('SELECT * FROM item WHERE id = ? AND list_id = ?', [request.params.itemID, request.params.listId], (error, row) => {
-    if (error) {
-      console.error(error.message);
-      response.sendStatus(500);
-      return;
-    }
-    if (row === undefined) {
-      response.sendStatus(404);
-      return;
-    }
-    response.send(row);
-  });
+  db.get(
+    `SELECT * FROM item 
+    INNER JOIN list ON list.id = item.list_id
+    WHERE item.id = ? AND item.list_id = ? AND list.user_id = ?`,
+    [request.params.itemID, request.params.listId, 1],
+    (error, row) => {
+      if (error) {
+        console.error(error.message);
+        response.sendStatus(500);
+        return;
+      }
+      if (!row) {
+        response.sendStatus(404);
+        return;
+      }
+      response.send(row);
+    },
+  );
 });
 // create item
 router.post('/:listId/items', (request, response) => {
-  db.run('INSERT INTO item(description, list_id) VALUES (?, ?)', [request.body.description, request.params.listId], function (error) {
-    if (error) {
-      console.error(error.message);
-      response.sendStatus(500);
-      return;
-    }
-    response.status(200).json(this.lastID);
-  });
+  db.get('SELECT id FROM list WHERE id = ? AND user_id = ?',
+    [request.params.listId, 1],
+    (checkError, row) => {
+      if (checkError) {
+        console.error(checkError.message);
+        response.sendStatus(500);
+        return;
+      }
+      if (!row) {
+        response.sendStatus(400);
+        return;
+      }
+      db.run('INSERT INTO item(description, list_id) VALUES (?, ?)',
+        [request.body.description, request.params.listId],
+        function (error) {
+          if (error) {
+            console.error(error.message);
+            response.sendStatus(500);
+            return;
+          }
+          response.status(200).json(this.lastID);
+        });
+    });
 });
 // update item by id
 router.put('/:listId/items/:itemId', (request, response) => {
-  db.run('UPDATE item SET status = ?, description = ? WHERE list_id = ? AND id = ?', [request.body.status, request.body.description, request.params.listId, request.params.itemId], (error) => {
-    if (error) {
-      console.error(error.message);
-      response.sendStatus(500);
-    }
-  });
-  response.sendStatus(200);
+  db.get(
+    `SELECT * FROM item 
+    INNER JOIN list ON list.id = item.list_id 
+    WHERE item.id = ? AND item.list_id = ? AND list.user_id = ?`,
+    [request.params.itemId, request.params.listId, 1],
+    (error, row) => {
+      if (error) {
+        console.error(error.message);
+        response.sendStatus(500);
+        return;
+      }
+      if (!row) {
+        response.sendStatus(404);
+        return;
+      }
+      db.run('UPDATE item SET status = ?, description = ? WHERE id = ?',
+        [
+          request.body.status ?? row.status,
+          request.body.description ?? row.description,
+          request.params.itemId,
+        ],
+        (err) => {
+          if (err) {
+            console.error(error.message);
+            response.sendStatus(500);
+          }
+          response.sendStatus(200);
+        });
+    },
+  );
 });
 // delete item by id
 router.delete('/:listId/items/:itemId', (request, response) => {
-  db.run('DELETE FROM items WHERE list_id = ?  AND id = ?', [request.params.listId, request.params.itemId], (error) => {
-    if (error) {
-      console.error(error.message);
-      response.sendStatus(500);
-    }
-  });
-  response.sendStatus(200);
+  db.run(
+    `DELETE FROM item 
+    WHERE item.id IN (
+      SELECT item.id
+      FROM item
+      INNER JOIN list ON item.list_id = list.id 
+      WHERE item.list_id = ? AND item.id = ? AND list.user_id = ?
+    )`,
+    [request.params.listId, request.params.itemId, 1],
+    (error) => {
+      if (error) {
+        console.error(error.message);
+        response.sendStatus(500);
+      }
+      response.sendStatus(200);
+    },
+  );
 });
 
 module.exports = router;
