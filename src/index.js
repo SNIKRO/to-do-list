a4e838f8e6fcb74d482c456042131c8eee40c005
SNIKRO <snikr@bk.ru>
2021-08-28 00:45:23 +0600

    Add registration users

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const listRouter = require('./controllers/lists');
 const itemRouter = require('./controllers/items');
+const userRouter = require('./controllers/users');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+app.use('/', userRouter);
 app.use('/lists', listRouter);
 app.use('/lists', itemRouter);
 
