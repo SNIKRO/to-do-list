1d5a8c5d4c668f2b0590fb49605c65b21e91e22e
SNIKRO <snikr@bk.ru>
2021-09-13 10:35:22 +0600

    Refactoring log-out, refresh

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,8 +1,4 @@
 const { Router } = require('express');
-const jwt = require('jsonwebtoken'); // access token
-const { v4: uuid } = require('uuid');// refresh token
-const config = require('../../../config.json');
-const db = require('../../db');
 const authMiddleware = require('../../middlewares/auth');
 const authService = require('../../services/auth');
 const ServiceError = require('../../errors/service');
@@ -48,55 +44,26 @@ router.post('/sign-up', (request, response) => {
 });
 
 router.post('/log-out', authMiddleware, (request, response) => {
-  db.run(
-    `DELETE FROM token 
-    WHERE user_id = ? 
-    `,
-    [request.user],
-    (error) => {
-      if (error) {
-        console.log(error.message);
-        response.sendStatus(500);
-        return;
-      }
-      response.sendStatus(200);
-    },
-  );
+  authService.logOut(request.user).then(() => {
+    response.sendStatus(200);
+  }).catch((error) => {
+    if (error instanceof ServiceError) {
+      response.status(400).send(error.message); // what mistake should there be and should it?
+      return;
+    }
+    response.sendStatus(500);
+  });
 });
 
 router.post('/refresh', authMiddleware, (request, response) => {
-  db.get(
-    `SELECT count(*) as count FROM token
-    WHERE user_id = ? AND token = ?
-    `,
-    [request.user, request.body.refreshToken],
-    (error, row) => {
-      if (error) {
-        console.log(error.message);
-        response.sendStatus(500);
-        return;
-      }
-      if (row.count === 0) {
-        response.sendStatus(401);
-        return;
-      }
-      const accessToken = jwt.sign({ userId: request.user }, config.KEY);
-      const refreshToken = uuid();
-      db.run(
-        `UPDATE token SET token = ?
-        WHERE user_id = ? AND token = ?
-        `,
-        [refreshToken, request.user, request.body.refreshToken],
-        (updateError) => {
-          if (updateError) {
-            console.log(updateError.message);
-            response.sendStatus(500);
-            return;
-          }
-          response.send({ accessToken, refreshToken });
-        },
-      );
-    },
-  );
+  authService.refresh(request.user, request.body.refreshToken).then((tokens) => {
+    response.send(tokens);
+  }).catch((error) => {
+    if (error instanceof ServiceError) {
+      response.status(401).send(error.message);
+      return;
+    }
+    response.sendStatus(500);
+  });
 });
 module.exports = router;
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -73,4 +73,65 @@ function signUp(name, email, password) {
     },
   );
 }
-module.exports = { signIn, signUp };
+
+function logOut(user) {
+  return new Promise(
+    (resolve, reject) => {
+      db.run(
+        `DELETE FROM token 
+        WHERE user_id = ? 
+        `,
+        [user],
+        (error) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve();
+        },
+      );
+    },
+  );
+}
+
+function refresh(user, oldRefreshToken) {
+  return new Promise(
+    (resolve, reject) => {
+      db.get(
+        `SELECT count(*) as count FROM token
+        WHERE user_id = ? AND token = ?
+        `,
+        [user, oldRefreshToken],
+        (error, row) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+          if (row.count === 0) {
+            reject(new ServiceError('User unauthorized'));
+            return;
+          }
+          const accessToken = jwt.sign({ userId: user }, config.KEY);
+          const refreshToken = uuid();
+          db.run(
+            `UPDATE token SET token = ?
+            WHERE user_id = ? AND token = ?
+            `,
+            [refreshToken, user, oldRefreshToken],
+            (updateError) => {
+              if (updateError) {
+                reject(updateError);
+                return;
+              }
+              resolve({ accessToken, refreshToken });
+            },
+          );
+        },
+      );
+    },
+  );
+}
+
+module.exports = {
+  signIn, signUp, logOut, refresh,
+};
