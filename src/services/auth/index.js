67a1f22c782b39667d7661edd72bd3458d0b864b
SNIKRO <snikr@bk.ru>
2021-09-22 10:21:57 +0600

    Refactoring services across repositories

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -4,34 +4,27 @@ const bcrypt = require('bcrypt');
 const config = require('../../../config.json');
 const db = require('../../db');
 const ServiceError = require('../../errors/service');
+const userRepo = require('../../repositories/user');
 
 function signIn(email, password) {
   return new Promise(
     (resolve, reject) => {
-      db.get(`SELECT id, password FROM user 
-      WHERE email = ? `,
-      [email],
-      (error, row) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        if (!row
-          || !bcrypt.compareSync(password, row.password)) {
-          reject(new ServiceError('Pair email/password is incorrect'));
-          return;
-        }
-        const accessToken = jwt.sign({ userId: row.id }, config.KEY);
-        const refreshToken = uuid();
-        db.run(`INSERT INTO token(user_id, token) 
+      const user = userRepo.getUser(email);
+      if (!user
+          || !bcrypt.compareSync(password, user.password)) {
+        reject(new ServiceError('Pair email/password is incorrect'));
+        return;
+      }
+      const accessToken = jwt.sign({ userId: user.id }, config.KEY);
+      const refreshToken = uuid();
+      db.run(`INSERT INTO token(user_id, token) 
           VALUES (?,?)`,
-        [row.id, refreshToken], (err) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve({ accessToken, refreshToken });
-        });
+      [user.id, refreshToken], (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve({ accessToken, refreshToken });
       });
     },
   );
