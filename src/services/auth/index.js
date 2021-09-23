73dbbe528eed5df375fa83586b3892ea36df39d3
SNIKRO <snikr@bk.ru>
2021-09-23 09:45:22 +0600

    Add repository for token, refactoring signIn

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -2,26 +2,21 @@ const jwt = require('jsonwebtoken'); // access token
 const { v4: uuid } = require('uuid');// refresh token
 const bcrypt = require('bcrypt');
 const config = require('../../../config.json');
-const db = require('../../db');
 const ServiceError = require('../../errors/service');
 const userRepo = require('../../repositories/user');
+const tokenRepo = require('../../repositories/tokenRepository');
 
-function signIn(email, password) {
-  return new Promise(
-    (resolve, reject) => {
-      const user = userRepo.getUser(email);
-      if (!user
-          || !bcrypt.compareSync(password, user.password)) {
-        reject(new ServiceError('Pair email/password is incorrect'));
-        return;
-      }
-      const accessToken = jwt.sign({ userId: user.id }, config.KEY);
-      const refreshToken = uuid();
+async function signIn(email, password) {
+  const user = await userRepo.getUser(email);
+  if (!user
+     || !bcrypt.compareSync(password, user.password)) {
+    return (new ServiceError('Pair email/password is incorrect'));
+  }
+  const accessToken = jwt.sign({ userId: user.id }, config.KEY);
+  const refreshToken = uuid();
 
-      userRepo.insertToken(user.id, refreshToken);
-      resolve({ accessToken, refreshToken });
-    },
-  );
+  tokenRepo.insertToken(user.id, refreshToken);
+  return ({ accessToken, refreshToken });
 }
 
 function signUp(email, password) {
